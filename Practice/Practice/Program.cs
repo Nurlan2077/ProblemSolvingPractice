c132c24897d1587b45b28b4ec21c64c88cd75825
Nurlan2077 <51267641+Nurlan2077@users.noreply.github.com>
2020-06-04 23:25:26 +0500

    Практика. Задания 1 и 2.

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountUFO = Int32.Parse(Console.ReadLine());        // Количество тарелок.
+            string[] laserInfo = Console.ReadLine().Split(' ');     // Строковые координаты луча.
+            int[] laserCoords = new int[laserInfo.Length];          // Числовые координаты луча.
+
+            // Перевод строковых координат в числовые.
+            for (int i = 0; i < laserInfo.Length; i++)
+            {
+                laserCoords[i] = int.Parse(laserInfo[i]);
+            }
+
+            // Координаты точек лазера.
+            int x1 = laserCoords[0];
+            int y1 = laserCoords[1];
+            int x2 = laserCoords[2];
+            int y2 = laserCoords[3];
+
+            double A = y2 - y1;
+            double B = x1 - x2;
+            double C = x2 * y1 - x1 * y2;
+
+            int count = 0;      // Вспомогательная переменная для подсчёта сбитых тарелок.
+            string res = "";    // Вспомог. переменная для вывода ответа.
+
+            int[] result = new int[amountUFO]; 
+
+            // Проходит по всем координатам тарелок.
+            for (int i = 1; i <= amountUFO; i++)
+            {
+                string[] infoUFO = Console.ReadLine().Split(' ');
+
+                // Переводит строковые координаты в числовые.
+                int x = Int32.Parse(infoUFO[0]);
+                int y = Int32.Parse(infoUFO[1]);
+                int R = Int32.Parse(infoUFO[2]);
+
+                // Проверяет расстояние между прямой и окружностью.
+                if (Math.Abs(A * x + B * y + C) / Math.Sqrt(A * A + B * B) <= R )
+                {
+                    result[count] = i;
+                    count++;
+                }
+            }
+
+            // Вывод ответа.
+            Console.WriteLine(count);
+
+            for (int i = 0; i < amountUFO; i++)
+            {
+                if(result[i] > 0)
+                {
+                    Console.Write(result[i] + " ");
+                }
+            }
+        }
+    }
+}
---
+++ Practice/Task2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ввод количества слов и ключей.
+            string[] input = Console.ReadLine().Split(' ');
+
+            // Приведение к числовому типу.
+            int wordsNum = Int32.Parse(input[0]);
+            int keysNum  = Int32.Parse(input[1]);
+
+            string words = "";      // Хранит все слова.
+
+
+            // Записывает все слова в одну строку.
+            for (int i = 0; i < wordsNum; i++)
+            {
+                words += Console.ReadLine();
+            }
+
+            string keys = "";       // Хранит все ключи.
+
+            // Записывает все ключи в одну строку.
+            for (int i = 0; i < keysNum; i++)
+            {
+                keys += Console.ReadLine();
+            }
+
+            // Проходит по каждому символу ключей.
+            for(int i = 0; i < keys.Length; i += 1)
+            {
+                // Если содержит символ ключа, удаляет.
+                if (words.Contains(keys[i].ToString()))
+                {
+                    int temp = words.IndexOf(keys[i].ToString());   // Хранит индекс удаляемого символа.
+                    words = words.Remove(temp, 1);                  // Удаляет символ из строки.
+                }
+            }
+
+            // Хранит итоговый массив с символами.
+            char[] result = words.ToCharArray();
+
+            // Сортирует символы в алфавитном порядке.
+            Array.Sort(result);
+            
+            // Вывод результата.
+            foreach(char elem in result)
+            {
+                Console.Write(elem);
+            }
+
+        }
+    }
+}
