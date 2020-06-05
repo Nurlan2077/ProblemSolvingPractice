4acf4a88feb9f5fa12c0e4cd8ed9eebe9ffd7ac0
Nurlan2077 <51267641+Nurlan2077@users.noreply.github.com>
2020-06-05 11:53:50 +0500

    Сделана задача 3.
    Задача 3 в консоли.

---
+++ Practice/Task3/Task3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class Task3
+    {
+        /// <summary>
+        ///  Метод для проверки принадлежности точки графику: y >= |x| и y >= 1.
+        /// </summary>
+        public static bool CheckPoint(double coordX, double coordY)
+        {
+            // Если попадает в область значений графика, то true, иначе false.
+            if (coordY >= Math.Abs(coordX) || coordY >= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Перегрузка метода для проверки на принадлежность графику.
+        public static bool CheckPoint(double y)
+        {
+            double x = 0;
+
+            // Если попадает в область значений графика, то true, иначе false.
+            if (y >= x || y >= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
