0bef1a7a1f708011335e4a638ace8c199b379c3c
Nurlan2077 <51267641+Nurlan2077@users.noreply.github.com>
2020-06-07 15:33:35 +0500

    Unit-тесты к задаче 7

---
+++ Practice/Task7_UnitTests/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Practic.Task7;
+
+namespace Task7_UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        // Проверка вычисления кода с единичным повторением.
+        public void TestCalculateCodeOneRepeat()
+        {
+            int[] lengths = { 1, 1 };
+
+            string[] actual = CalculateCode(lengths);
+
+            string[] expected = { "b", "c" };
+
+            CollectionAssert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        // Проверка вычисления кода с множественным повторением.
+        public void TestCalculateCodeMultipleRepeat()
+        {
+            int[] lengths = { 3, 2, 2, 2 };
+
+            string[] actual = CalculateCode(lengths);
+
+            string[] expected = { "ab", "ba", "baa", "ca" };
+
+            CollectionAssert.AreEqual(actual, expected);
+        }
+
+
+        [TestMethod]
+        // Проверка вычисления кода без повторений.
+        public void TestCalculateCodeZeroRepeat()
+        {
+            int[] lengths = { 1, 2, 3, 4 };
+
+            string[] actual = CalculateCode(lengths);
+
+            string[] expected = { "b", "ba", "baa", "baaa" };
+
+            CollectionAssert.AreEqual(actual, expected);
+        }
+
+
+        [TestMethod]
+        // Проверка вычисления кода без повторений.
+        public void TestCheckMacmillan()
+        {
+            int[] lengths = { 1, 1, 1, 1 };
+
+            bool actual = CheckMacmillan(3, lengths);
+
+            bool expected = false;
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
