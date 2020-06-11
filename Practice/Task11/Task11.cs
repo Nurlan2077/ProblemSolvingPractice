53da559c2be55c6807f6c06e7905fbe75aa40b45
Nurlan2077 <51267641+Nurlan2077@users.noreply.github.com>
2020-06-11 22:17:30 +0500

    Задача 11. Добавлено демо.

---
+++ Practice/Task11/Task11.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Task11
+    {
+        // Метод для дешифрования последовательности.
+        public static int[] Decode(int[] line)
+        {
+            List<int> result = new List<int>();     // Хранит результат.
+
+            // Выбрасывает исключение, если кол-во цифр в последовательности не кратно 3.
+            if (line.Length % 3 != 0)
+            {
+                throw new Exception("В последовательности недостает цифр.");
+            }
+
+            int sum = 0;    // Вспомог. перемен. хранит сумму трех элем-ов послед-ти.
+
+            // Проходит по всем цифрам.
+            for(int i = 0; i < line.Length; i += 1)
+            {
+                sum += line[i];     // Прибавляет цифру к сумме.
+
+                // Если три цифры пройдено, проверяет сумму.
+                if((i + 1) % 3 == 0 && sum < 2)
+                {
+                    result.Add(0);  // 0, если сумма меньше 2.
+                    sum = 0;        // Обнуляет счётчик суммы.
+                }
+                else if ((i + 1) % 3 == 0 && sum >= 2)
+                {
+                    result.Add(1);  // 1, если сумма больше 2.
+                    sum = 0;        // Обнуляет счётчик суммы.
+                }
+
+            }
+
+            // Возвращает массив с цифрами.
+            return result.ToArray();
+        }
+
+        // Доп. метод для перевода из двоичной системы счисления в десятичную.
+        public static int ConvertBinaryToInt10(int[] input)
+        {
+            string bin = "";    // Хранит цифры последовательности.
+
+            // Переводит цифры последовательности в строку.
+            foreach(int elem in input)
+            {
+                bin += elem;
+            }
+
+            // Конвертирует в десятичную.
+            int result = Convert.ToInt32(bin, 2);
+
+            return result;
+        }
+
+        // Доп. метод для перевода из двоичной системы счисления в 16-ричную.
+        public static string ConvertBinaryToInt16(int[] input)
+        {
+            int num = ConvertBinaryToInt10(input);          // Конвертирует в десятичную.
+
+            string result = Convert.ToString(num, 16);      // Конвертирует из десятичн. в 16-ричную.
+
+            return result;
+        }
+
+
+        // Метод, который выводит значения массива через пробел.
+        public static void ShowMas(int[] mas)
+        {
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write($"{mas[i]} ");
+            }
+
+            if (mas.Length == 0)
+            {
+                Console.WriteLine("Ваш массив пуст");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Practice/Task11/Task11_Demo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Practice.Task11;
+
+
+namespace Practice
+{
+    class Task11_Demo
+    {
+        static void Main(string[] args)
+        {
+            // Демонстрация дешифрования.
+
+            Console.WriteLine("Ввод последовательности:");
+            int[] input = { 1, 1, 1, 0, 1, 0, 1, 0, 1 };
+            int[] output = Decode(input);
+            ShowMas(output);
+
+            Console.WriteLine();
+            Console.WriteLine("Ввод пустой последовательности:");
+            input = new int[] { };
+            output = Decode(input);
+            ShowMas(output);
+
+            Console.WriteLine();
+            Console.WriteLine("Ввод последовательности c 1 элементом:");
+            input = new int[] { 1};
+            output = Decode(input);
+            ShowMas(output);
+
+
+        }
+    }
+}
