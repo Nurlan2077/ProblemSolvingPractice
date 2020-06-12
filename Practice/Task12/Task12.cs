787425b589b180278feb83e70537edd121783305
Nurlan2077 <51267641+Nurlan2077@users.noreply.github.com>
2020-06-12 23:13:14 +0500

    Решение задачи 12 с доп. методом сортировки

---
+++ Practice/Task12/Task12.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    // Реализация методов сортировки вставками, выбором и подсчётом.
+    public class Task12
+    {
+        // Сортировка вставками.
+        public static int[] InsertionSort(int[] input)
+        {
+            int countTransfer = 0;      // Количество перемещений элементов массива.
+            int countComparison = 0;    // Количество сравнений.
+
+            // Проходит по всем элементам массива.
+            for (int actualCursor = 1; actualCursor < input.Length; actualCursor++)
+            {
+                int unsortedCursor;
+
+                int temp = input[actualCursor];  // Вспомог. перемен. хранит текущ. элемент.
+
+                // Проходит по несортированной части.
+                for (unsortedCursor = actualCursor - 1; unsortedCursor >= 0; unsortedCursor--)
+                {
+
+                    countComparison++;
+
+                    // Если предыдущ. элемент, не меньше текущ., 
+                    // то прерывает цикл.
+                    if (input[unsortedCursor] < temp)
+                    {
+                        break;
+                    }
+
+                    // Меняет след. элемент с текущим.
+                    input[unsortedCursor + 1] = input[unsortedCursor];
+                    countTransfer++;
+                }
+
+                // Вставляет меньший найденный элемент.
+                input[unsortedCursor + 1] = temp;
+                countTransfer++;
+
+            }
+
+            Console.WriteLine($"Количество сравнений: {countComparison}");
+            Console.WriteLine($"Количество пересылок: {countTransfer}");
+
+            return input;
+        }
+
+        public static int[] SelectionSort(int[] input)
+        {
+            int min;                    // Хранит мин. значение.
+            int temp;                   // Вспомог. перемен. для хранения предыдущ. элемента.
+            int countTransfer = 0;      // Количество перемещений элементов массива.
+            int countComparison = 0;    // Количество сравнений.
+
+
+            // Проходит по всем элементам массива.
+            for (int sortedCursor = 0; sortedCursor < input.Length - 1; sortedCursor++)
+            {
+
+                min = sortedCursor;    // Текущий элемент становится минимальным.
+
+                countTransfer++;
+
+                // Проходит по остальным элементам массива.
+                for (int unsortedCursor = sortedCursor + 1; unsortedCursor < input.Length; unsortedCursor++)
+                {
+                    countComparison++;
+
+                    // Если след. элемент меньше текущего минимума,
+                    // то заменяет.
+                    if (input[unsortedCursor] < input[min])
+                    {
+                        min = unsortedCursor;
+
+                        countTransfer++;
+
+                    }
+                }
+
+                // Если минимум не равен текущему элементу,
+                // меняет местами текущий элемент и минимальный.
+
+                countComparison++;
+
+                if (min != sortedCursor)
+                {
+                    temp = input[sortedCursor];
+                    input[sortedCursor] = input[min];
+                    input[min] = temp;
+
+                    countTransfer += 3;
+                }
+            }
+
+
+            Console.WriteLine($"Количество сравнений: {countComparison}");
+            Console.WriteLine($"Количество пересылок: {countTransfer}");
+
+            return input;
+        }
+
+
+        // Дополнительный метод для сортировки подсчётом.
+        public static int[] CountingSort(int[] input)
+        {
+            int countTransfer = 0;      // Количество перемещений элементов массива.
+            int countComparison = 0;    // Количество сравнений.
+
+            // Вспомог. переменная.
+            int[] temp = new int[input.Length];
+
+            
+            int min = input[0];  // Минимальное значение массива.
+            int max = input[0];  // Максимальное значение.
+
+            // Проходит по всем элементам массива,
+            // находит мин и макс значение.
+            for (int i = 1; i < input.Length; i++)
+            {   
+                if (input[i] < min)
+                {
+                    min = input[i];
+                }
+                else if (input[i] > max)
+                {
+                    max = input[i];
+                    countComparison++;
+                }
+
+                countComparison++;
+
+            }
+
+            // Массив для подсчёта.
+            int[] counts = new int[max - min + 1];
+
+            // Заполняет массив для подсчёта числами от мин до макс.
+            for (int i = 0; i < input.Length; i++)
+            {
+                counts[input[i] - min]++;
+            }
+
+            // Декрементирует первый элемент.
+            counts[0]--;
+
+            // Заполняет массив для подсчёта.
+            for (int i = 1; i < counts.Length; i++)
+            {
+                counts[i] = counts[i] + counts[i - 1];
+                countTransfer++;
+            }
+
+            // Сортирует массив.
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                temp[counts[input[i] - min]--] = input[i];
+                countTransfer++;
+            }
+
+            Console.WriteLine($"Количество сравнений: {countComparison}");
+            Console.WriteLine($"Количество пересылок: {countTransfer}");
+
+            return temp;
+        }
+
+
+
+
+        // Метод, который выводит значения массива через пробел.
+        public static void ShowMas(int[] mas)
+        {
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write($"{mas[i]} ");
+            }
+
+            if (mas.Length == 0)
+            {
+                Console.WriteLine("Ваш массив пуст");
+            }
+
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Practice/Task12/Task12_Demo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Practice.Task12;
+
+namespace Practice
+{
+    class Task12_Demo
+    {
+        // Демонстрация различных видов сортировки массивов.
+        static void Main(string[] args)
+        {
+            int[] chaotic = new int[] { 2, -1, 0, 127, 14, 13, 23, 8, 7, -2 };  // Неупорядоченный массив.
+            int[] rising = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };         // По возрастанию.
+            int[] declining = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };      // По убыванию.
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Cортировка подсчётом: ");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine();
+
+            Console.WriteLine("По возрастанию:");
+            ShowMas(CountingSort(rising));
+            Console.WriteLine("По убыванию:");
+            ShowMas(CountingSort(declining));
+            Console.WriteLine("Неупорядоченный:");
+            ShowMas(CountingSort(chaotic));
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Сортировка вставками: ");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine();
+
+            Console.WriteLine("По возрастанию:");
+            ShowMas(InsertionSort(rising));
+            Console.WriteLine("По убыванию:");
+            ShowMas(InsertionSort(declining));
+            Console.WriteLine("Неупорядоченный:");
+            ShowMas(InsertionSort(chaotic));
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Сортировка выбором: ");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine();
+
+            Console.WriteLine("По возрастанию:");
+            ShowMas(SelectionSort(rising));
+            Console.WriteLine("По убыванию:");
+            ShowMas(SelectionSort(declining));
+            Console.WriteLine("Неупорядоченный:");
+            ShowMas(SelectionSort(chaotic));
+
+            Console.WriteLine();
+        }
+    }
+}
