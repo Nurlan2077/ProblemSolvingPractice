6f100cf51533cbd677f49c907be0e2ffd0667bed
Nurlan2077 <51267641+Nurlan2077@users.noreply.github.com>
2020-06-13 09:27:31 +0500

    Добавлена демонстрационная программа.

---
+++ Practice/Task9/Task9.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Task9
+    {
+        // Вычисление разности суммы чётных и суммы нечётных чисел в кольцевом списке.
+        public static int CalculateDifference(RingList ring)
+        {
+            Node current = ring.head;    // Указывает на головной элемент.
+
+            int sumEvens = 0;
+            int sumOdds = 0;
+
+            // Пока не дойдет до хвостого элемента,
+            // выводит элементы.
+            while (current != null)
+            {
+                if (current.data % 2 == 0)
+                {
+                    sumEvens += current.data;
+                }
+                else
+                {
+                    sumOdds += current.data;
+                }
+
+                current = current.next;
+            }
+
+            return sumEvens - sumOdds;
+        }
+
+
+        // Класс узла кольцевого списка.
+        public class Node
+        {
+            public Node next;   // Информация о следующем элементе списка.
+            public int data;    // Число в узле.
+        }
+
+
+        // Класс кольцевого списка.
+        public class RingList
+        {
+            public Node head;   // Ссылка на головной элемент списка.
+
+
+            // Добавление нового узла в начало.
+            public void AddFirst(int data)
+            {
+                Node addNode = new Node();
+
+                addNode.data = data;
+                addNode.next = head;
+
+                head = addNode;
+            }
+
+            // Добавление узла в конец списка.
+            public void AddLast(int data)
+            {
+                // Если нет головного элемента.
+                if (head == null)
+                {
+                    head = new Node();
+
+                    head.data = data;
+                    head.next = null;
+                }
+                else
+                {
+                    Node toAdd = new Node();
+                    toAdd.data = data;
+
+                    Node current = head;
+                    while (current.next != null)
+                    {
+                        current = current.next;
+                    }
+
+                    current.next = toAdd;
+                }
+            }
+
+
+            // Вывод всех узлов.
+            public void showNodes()
+            {
+                Node current = head;    // Указывает на головной элемент.
+
+                // Пока не дойдет до хвостого элемента,
+                // выводит элементы.
+                while (current != null)
+                {
+                    Console.WriteLine(current.data);
+                    current = current.next;
+                }
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ Practice/Task9/Task9_Demo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static Practice.Task9;
+
+namespace Task9
+{
+    // Демонстрация решения задачи 9.
+    class Task9_Demo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целые числа через пробел.");
+
+            int[] nums = new int[0];
+
+            bool ok = false;
+
+            // Проверка ввода массива.
+            do
+            {
+                try
+                {
+                    string input = Console.ReadLine();
+                    nums = Regex.Split(input.Trim(), @"\s+").Select(Int32.Parse).ToArray();
+                    ok = true;
+                }
+                catch (System.FormatException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Введите только целые числа.");
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                }
+            }
+            while (!ok);
+
+            RingList ringList = new RingList();
+
+            // Заполнение кольцевого списка.
+            foreach (int num in nums)
+            {
+                ringList.AddLast(num);
+            }
+
+            Console.WriteLine($"Разность суммы чётных и суммы нечётных чисел равна: {CalculateDifference(ringList)}");
+
+            Console.ReadLine();
+        }
+    }
+}
