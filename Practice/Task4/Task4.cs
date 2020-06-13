61ee2175fe8e6d77cd61c8fc96c3342dc3ced363
Nurlan2077 <51267641+Nurlan2077@users.noreply.github.com>
2020-06-13 13:11:37 +0500

    Добавлен интерфейс, добавление границ для метода

---
+++ Practice/Task4/Task4.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class Task4
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите точность вычисления:");
+                double eps = CheckUserInput();
+
+                Console.WriteLine(FindRoot(eps, 2.1, 2.2));
+            }
+        }
+
+
+        // Метод для поиска корня уравнения.
+        public static double FindRoot(double eps, double left, double right)
+        {
+            double prevNum = left;  // Хранит предыдущее значение последовательности.
+            double actualNum;       // Текущее значение послед-ти.
+            double temp;            // Вспомогательная переменная.
+
+            do
+            {
+                // Высчитывает текущее значение.
+                actualNum = prevNum - (Math.Pow(prevNum, 3) - 2 * Math.Pow(prevNum, 2) + prevNum - 3)
+                                       /(3*Math.Pow(prevNum, 2) - 4*prevNum + 1);
+
+                Console.WriteLine("Предыдущее число:" + prevNum);
+                temp = prevNum;         // Запоминает предыдущее значение для высчитывания разницы.
+                prevNum = actualNum;    // Записывает в предыдущее текущее.
+
+                Console.WriteLine("Текущее число:" + actualNum);
+                Console.WriteLine("Разница:" + Math.Abs(actualNum - temp));
+                Console.WriteLine();
+
+            }
+            while (Math.Abs(actualNum - temp) > eps && Math.Abs(actualNum - temp) < right);   // Если модуль разницы пред. и текущ. больше точности,
+                                                        // то прекращает вычисление.
+            
+            // Возвращает последнее вычисленное значение.
+            return actualNum;
+        }
+
+
+        // Метод для проверки ввода пользователя.
+        public static double CheckUserInput()
+        {
+            double num;         // Целое число, введенное пользователем.
+            string input;       // Введенная строка.
+            do
+            {
+                input = Console.ReadLine();
+
+                // Проверка ввода, для вывода предупреждения.
+                if (!double.TryParse(input, out _))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Введите действительное число.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+            while (!double.TryParse(input, out num)); // Проверка ввода, если введено целое число, то выходит из цикла.
+            return num;
+        }
+    }
+}
---
+++ Practice/Task4_App/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static Practice.Task4;
+
+namespace Task4_App
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        // Вычисляет значение по нажатие на кнопку.
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            // Проверяет поля на пустоту.
+            if (AccuracyTextBox.Text.Trim() == ""
+                || LeftBorder.Text.Trim()   == ""
+                || RightBorder.Text.Trim()  == "")
+            {
+                MessageBox.Show("Заполните все поля вещественными числами, для разделения используйте ','.");
+            }
+            else
+            {
+                // Вычисляет значение с заданной точностью и границами.
+                Result.Content = FindRoot(double.Parse(AccuracyTextBox.Text),
+                                 double.Parse(LeftBorder.Text),
+                                 double.Parse(RightBorder.Text));
+            }
+
+        }
+
+
+        // Ограничивает ввод пользователя только вещественными числами.
+        private void AccuracyTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex(@"^[0-9]*(?:\,[0-9]*)?$");
+            e.Handled = !regex.IsMatch(e.Text);
+        }
+    }
+}
