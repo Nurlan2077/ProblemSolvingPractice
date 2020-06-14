0e2583fdf97579365cc8ddf5c809346daa90125d
Nurlan2077 <51267641+Nurlan2077@users.noreply.github.com>
2020-06-14 19:47:30 +0500

    Рефакторинг, добавление демо

---
+++ Practice/Task5/Task5.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class Task5
+    {
+        // Метод для вычисления суммы элементов до последнего отрицательного числа в строке матрицы.
+        public static int[] FindSum(int[][] matr)
+        {
+            int size = matr.Length;             // Размерность матрицы.
+
+            int[] result = new int[size];       // Ответ с суммой строк.
+            
+            // Проходит по каждой строке матрицы.
+            for(int i = 0; i < size; i++)
+            {
+                int sum = 0;                      // Сумма строки.
+                int prevSum = 0;                  // Вспомог. переменная. Хранит предыдущую сумму.
+                bool wasNegative = false;         // Вспомог. перемен. Хранит значение, было ли отриц. число в строке.
+
+                // Проходит по каждой ячейке строки.
+                for (int j = 0; j < matr[i].Length; j++)
+                {
+                    sum += matr[i][j];  // Суммирует значения.
+                    
+                    // Проверяет наличие отриц. числа в ячейке.
+                    if (matr[i][j] < 0)
+                    {
+                        wasNegative = true;
+                        prevSum = sum - matr[i][j];
+                    }
+                }
+
+                // Если не было отриц. чисел в строке,
+                // записывает -1.
+                if (wasNegative)    result[i] = prevSum;
+                else                result[i] = -1;
+            }
+
+            return result;
+        }
+
+
+        // Метод для вывода ступенчатых массивов.
+        public static void ShowMas(int[][] mas)
+        {
+            int lenght = mas.Length;    // Количество массивов в ступенчатом массиве.
+
+            for (int i = 0; i < lenght; i++)
+            {
+                for (int j = 0; j < mas[i].Length; j++)
+                {
+                    Console.Write($"{mas[i][j]} ");
+                }
+
+                Console.WriteLine();
+            }
+
+            if (mas.Length == 0)
+            {
+                Console.WriteLine("Ваш массив пуст");
+            }
+
+            Console.WriteLine();
+        }
+
+
+        // Метод, проверяющий ввод пользователя.
+        public static int CheckUserInput()
+        {
+            int num;        // Целое число, введенное пользователем.
+            string input;   // Введенная строка.
+            do
+            {
+                input = Console.ReadLine();
+
+                // Проверка ввода, для вывода предупреждения.
+                if (!int.TryParse(input, out _))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Введите целое число.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+            while (!int.TryParse(input, out num));  // Проверка ввода, если введено целое число, то выходит из цикла.
+            return num;
+        }
+    }
+}
---
+++ Practice/Task5/Task5_Demo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Practice.Task5;
+
+namespace Practice
+{
+    class Task5_Demo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность массива:");
+
+            // Размерность матрицы.
+            int size = CheckUserInput();
+
+            int[][] matr = new int[size][];
+            Random rand = new Random();
+
+            // Случайная генерация элементов матрицы.
+            for (int i = 0; i < size; i++)
+            {
+                matr[i] = new int[size];
+                for (int j = 0; j < size; j++)
+                {
+                    matr[i][j] = rand.Next(-100, 100);
+                }
+            }
+
+            // Вывод матрицы.
+            ShowMas(matr);
+
+            // Вычисление суммы.
+            int[] res = FindSum(matr);
+
+            // Вывод элемента.
+            foreach (int elem in res)
+            {
+                Console.WriteLine(elem);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
