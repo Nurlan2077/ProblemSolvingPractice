f7d16d37a3a87dbd547b6a17caaff23ba08e4a17
Nurlan2077 <51267641+Nurlan2077@users.noreply.github.com>
2020-06-14 11:11:06 +0500

    Рефакторинг, изменения в Demo

--- Practice/Task3/Task3.cs
+++ Practice/Task3/Task3.cs
@@ -39,5 +39,26 @@ namespace Practice
                 return false;
             }
         }
+
+        // Метод для проверки ввода пользователя.
+        public static double CheckUserInput()
+        {
+            double num;         // Число, введенное пользователем.
+            string input;       // Введенная строка.
+            do
+            {
+                input = Console.ReadLine();
+
+                // Проверка ввода, для вывода предупреждения.
+                if (!double.TryParse(input, out _))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Введите действительное число.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+            while (!double.TryParse(input, out num)); // Проверка ввода, если введено целое число, то выходит из цикла.
+            return num;
+        }
     }
 }
---
+++ Practice/Task3/Task3_Demo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Practice.Task3;
+
+namespace Practice
+{
+    // Демонстрация программы для проверки точки на принадлежность графику y >= |x| и y >= 1.
+    class DemoTask3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число X:");
+            double pointX = CheckUserInput();       // Координата точки X.
+
+            Console.WriteLine("Введите число Y:");
+            double pointY = CheckUserInput();       // Координата точки Y.
+
+            // Проверка точек на принадлежность графику.
+            if (CheckPoint(pointX, pointY))
+            {
+                Console.WriteLine($"Точка ({pointX};{pointY}) принадлежит графику.");
+            }
+            else
+            {
+                Console.WriteLine($"Точка ({pointX};{pointY}) не принадлежит графику.");
+            }
+        }
+    }
+
+
+}
